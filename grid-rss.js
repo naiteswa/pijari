605b73faf9a5318487cf0bfc1800e345c7298378
naiteswa <103042073+naiteswa@users.noreply.github.com>
2022-04-09 21:15:12 +0700

    Add files via upload

---
+++ grid-rss.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+  loadRSS(
+    "https://www.pijari.com/rss.xml",
+    "#newsfeed",
+    "Pijari.com"
+  );
+});
+
+function loadRSS(link, htmlContainer, author) {
+  var url = link;
+  var container = $(htmlContainer);
+
+  feednami.load(url, function (result) {
+    if (result.error) {
+      console.log(result.error);
+    } else {
+      var entries = result.feed.entries;
+      for (var i = 0; i < 5; i++) {
+        var entry = entries[i];
+        container.append(
+          '<li class="rsscard"><p><h2>' +
+            '<a href="' +
+            entry.link +
+            '" target="_blank">' +
+            entry.title +
+            "</a>" +
+            "</h2></p>" +
+            author +
+            "</li>"
+        );
+      }
+    }
+  });
+}
