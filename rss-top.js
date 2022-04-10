cb9b05a41c939bb002e9b4429db17e9534987f99
naiteswa <103042073+naiteswa@users.noreply.github.com>
2022-04-10 11:13:51 +0700

    Add files via upload

---
+++ rss-top.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    loadRSS('https://www.pijari.com/atom.xml', '#Newsfeed', 'Pijari.com');   
+});
+
+function loadRSS(link, htmlContainer, author) {
+    var url = link;
+    var container = $(htmlContainer);
+
+    feednami.load(url, function(result){
+        if (result.error) {
+            console.log(result.error);
+        } else {
+            var entries = result.feed.entries;
+            for(var i = 0; i < 5; i++){
+                var entry = entries[i];
+                container.append("<li class=\"RSScard\"><p><h2>" 
+                    + "<a href=\"" + entry.link + "\" target=\"_blank\">" + entry.title + "</a>" 
+                    + "</h2></p>"+ author + "</li>");
+            }
+        }
+    });
+}
